ed5617049568e3ed323de9c99d6544b8e3252a26
Karma Drukpa <karmadrukpa11@gmail.com>
2021-01-01 22:24:34 -0500

    Change monogo url

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import users from './data/users';
-import products from './data/products';
+import users from './data/users.js';
+import products from './data/products.js';
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
@@ -12,7 +12,7 @@ dotenv.config();
 
 connectDB();
 
-const importDatat = async () => {
+const importData = async () => {
 	try {
 		await Order.deleteMany();
 		await Product.deleteMany();
@@ -27,5 +27,30 @@ const importDatat = async () => {
 			};
 		});
 		await Product.insertMany(sampleProducts);
-	} catch (error) {}
+		console.log('Data imported'.green.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(`${error}`.red.inverse);
+		process.exit(1);
+	}
 };
+
+const destoryData = async () => {
+	try {
+		await Order.deleteMany();
+		await Product.deleteMany();
+		await User.deleteMany();
+
+		console.log('Data Destroyed!'.red.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(`${error}`.red.inverse);
+		process.exit(1);
+	}
+};
+
+if (process.argv[2] === '-d') {
+	destoryData();
+} else {
+	importData();
+}
